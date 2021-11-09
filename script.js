f3dbdeb95eff9e131799c1c4b80f11948f914f83
Max Almroth <max.almroth95@gmail.com>
2021-11-09 19:31:02 +0100

    Added functionality and hover effect to buttons

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 // ===*VARIABLES*=== //
 var comma = false;
+const operators = [];
+const numbers = [];
 
 // ===*FUNCTIONS*=== //
 
@@ -28,8 +30,96 @@ function buttonNumber(num) {
     }
 }
 
-// Reset button
+// Operator buttons
+function buttonOperator(operator) {
+    var input = document.getElementById("input");
+    var calc = document.getElementById("calculation");
+
+    // Make sure new number isn't empty and fits into the calculation
+    if (input.innerHTML != "") {
+        numbers.push(parseFloat(input.innerHTML));
+        
+        if (calc.innerText.length + input.innerText.length + 3 < 80) {
+            switch (operator) {
+                case "÷":
+                    calc.innerHTML += ` ${input.innerHTML} ÷`;
+                    operators.push("÷");
+                    break;
+                
+                case "×":
+                    calc.innerHTML += ` ${input.innerHTML} ×`;
+                    operators.push("×");
+                    break;
+                
+                case "−":
+                    calc.innerHTML += ` ${input.innerHTML} −`;
+                    operators.push("−");
+                    break;
+                
+                case "+":
+                    calc.innerHTML += ` ${input.innerHTML} +`;
+                    operators.push("+");
+                    break;
+            }
+        }
+    }
+    else {
+        calc.innerHTML = calc.innerHTML.substring(0, calc.innerText.length)
+        operators.pop();
+
+        switch (operator) {
+            case "÷":
+                calc.innerHTML += "÷";
+                operators.push("÷");
+                break;
+            
+            case "×":
+                calc.innerHTML += "×";
+                operators.push("×");
+                break;
+            
+            case "−":
+                calc.innerHTML += "−";
+                operators.push("−");
+                break;
+            
+            case "+":
+                calc.innerHTML += "+";
+                operators.push("+");
+                break;
+        }
+    }
+
+    input.innerHTML = "";
+    comma = false;
+}
+
+// Equals button (solve equation)
+function buttonEquals() {
+    
+}
+
+// Remove button
+function remove() {
+    var input = document.getElementById("input");
+
+    input.innerHTML = input.innerHTML.substring(0, input.innerText.length - 1);
+
+    // Check if comma exists
+    if (input.innerHTML.indexOf(",") == -1)
+    {
+        comma = false;
+    }
+}
+
+// Reset buttons
 function reset() {
     document.getElementById("input").innerHTML = "";
+    comma = false;
+}
+
+function resetFull() {
+    document.getElementById("input").innerHTML = "";
+    document.getElementById("calculation").innerHTML = "";
     comma = false;
 }
