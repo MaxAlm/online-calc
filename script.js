16b1390afb5c3e753516a3a24feaa42c0b66a53c
Max Almroth <max.almroth95@gmail.com>
2021-11-14 10:23:04 +0100

    Added calculation algorithm and fixed some minor issues

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 // ===*VARIABLES*=== //
 var maxLength = 60;     // Controls max length of equation allowed 
 var comma = false;      // Keeps track of if comma has been entered or not
-const operators = [];
-const numbers = [];
+var equals = false;
+let operators = [];
+let numbers = [];
 
 // ===*FUNCTIONS*=== //
 
@@ -10,6 +11,16 @@ const numbers = [];
 // Number button
 function buttonNumber(num) {
     var input = document.getElementById("input");
+
+    // If equals
+    if (equals) {
+        input.innerHTML= "";
+        document.getElementById("calculation").innerHTML = "";
+        numbers = [];
+        operators = [];
+        comma = false;
+        equals = false;
+    }
 
     // If input isn't too long
     if (input.innerText.length < 16) {
@@ -33,87 +44,189 @@ function buttonNumber(num) {
 
 // Operator buttons
 function buttonOperator(operator) {
-    var input = document.getElementById("input");
-    var calc = document.getElementById("calculation");
+    if (document.getElementById("input").innerHTML != "Error!") {
+        var input = document.getElementById("input");
+        var calc = document.getElementById("calculation");
 
-    // Make sure new number isn't empty and fits into the calculation
-    if (input.innerText != "") {
-        let num = parseFloat(input.innerHTML.replace(",", "."));
-        numbers.push(num);
+        // If equals
+        if (equals) {
+            document.getElementById("calculation").innerHTML = "";
+            numbers = [];
+            operators = [];
+            equals = false;
+        }
 
-        if (calc.innerText.length + input.innerText.length + 3 <= maxLength) {
+        // Make sure new number isn't empty and fits into the calculation
+        if (input.innerText != "") {
+            let num = parseFloat(input.innerHTML.replace(",", "."));
+            numbers.push(num);
+
+            if (calc.textContent.length + input.textContent.length + 3 <= maxLength) {
+                switch (operator) {
+                    case "×":
+                        calc.innerHTML += ` ${num.toString().replace(".", ",")} ×`;
+                        operators.push("×");
+                        break;
+
+                    case "÷":
+                        calc.innerHTML += ` ${num.toString().replace(".", ",")} ÷`;
+                        operators.push("÷");
+                        break;
+
+                    case "+":
+                        calc.innerHTML += ` ${num.toString().replace(".", ",")} +`;
+                        operators.push("+");
+                        break;
+                    
+                    case "−":
+                        calc.innerHTML += ` ${num.toString().replace(".", ",")} −`;
+                        operators.push("−");
+                        break;
+                }
+            }
+            else {
+                return;
+            }
+        }
+        else {
+            calc.textContent = calc.textContent.substring(0, calc.textContent.length - 1);
+            operators.pop();
+
             switch (operator) {
                 case "÷":
-                    calc.innerHTML += ` ${num.toString().replace(".", ",")} ÷`;
+                    calc.innerHTML += "÷";
                     operators.push("÷");
                     break;
                 
                 case "×":
-                    calc.innerHTML += ` ${num.toString().replace(".", ",")} ×`;
+                    calc.innerHTML += "×";
                     operators.push("×");
                     break;
                 
                 case "−":
-                    calc.innerHTML += ` ${num.toString().replace(".", ",")} −`;
+                    calc.innerHTML += "−";
                     operators.push("−");
                     break;
                 
                 case "+":
-                    calc.innerHTML += ` ${num.toString().replace(".", ",")} +`;
+                    calc.innerHTML += "+";
                     operators.push("+");
                     break;
             }
         }
-        else {
-            return;
-        }
-    }
-    else {
-        calc.innerText = calc.innerText.substring(0, calc.innerText.length - 1);
-        operators.pop();
 
-        switch (operator) {
-            case "÷":
-                calc.innerHTML += "÷";
-                operators.push("÷");
-                break;
-            
-            case "×":
-                calc.innerHTML += "×";
-                operators.push("×");
-                break;
-            
-            case "−":
-                calc.innerHTML += "−";
-                operators.push("−");
-                break;
-            
-            case "+":
-                calc.innerHTML += "+";
-                operators.push("+");
-                break;
-        }
+        input.innerHTML = "";
+        comma = false;
     }
-
-    input.innerHTML = "";
-    comma = false;
 }
 
 // Equals button (solve equation)
 function buttonEquals() {
+    var input = document.getElementById("input");
+    var calc = document.getElementById("calculation");
     
+    // Set up arrays etc. for equation algorithm
+    if (!equals && numbers.length != 0) {
+        if (input.innerHTML != "") {
+            let num = parseFloat(input.innerHTML.replace(",", "."));
+            numbers.push(num);
+
+            calc.innerHTML += " " + input.innerHTML + " =";
+            equals = true;
+        } else {
+            // If only one number is stored
+            if (numbers.length == 1) {
+                input.innerHTML = numbers[0].toString().replace(".", ",");
+                calc.innerHTML = "";
+                numbers = [];
+                operators = [];
+                
+                // Check if comma exists
+                if (input.innerHTML.includes(",")) {
+                    comma = true;
+                }
+
+                return;
+            } else {
+                calc.textContent = calc.textContent.substring(0, calc.textContent.length - 1) + "=";
+                operators.pop();
+                equals= true;
+            }
+        }
+    }
+
+    // Calculate...
+    for (let k = 0; k < 4; k++) {
+        for (let i = operators.length - 1; i >= 0; i--) {
+            switch (k) {
+                case 0: // Multiply
+                    if (operators[i] == "×") {
+                        numbers[i] = numbers[i] * numbers[i + 1];
+                        operators.splice(i, 1);
+                        numbers.splice(i + 1, 1);
+                    }
+                    break;
+
+                case 1: // Divide
+                    if (operators[i] == "÷") {
+                        if (numbers[i + 1] != 0) {
+                            numbers[i] = numbers[i] / numbers[i + 1];
+                            operators.splice(i, 1);
+                            numbers.splice(i + 1, 1);
+                        } else {
+                            input.innerHTML = "Error!";
+                            equals = true;
+                            return;
+                        }
+                    }
+                    break;
+
+                case 2: // Add
+                    if (operators[i] == "+") {
+                        numbers[i] = numbers[i] + numbers[i + 1];
+                        operators.splice(i, 1);
+                        numbers.splice(i + 1, 1);
+                    }
+                    break;
+
+                case 3: // Subtract
+                    if (operators[i] == "−") {
+                        numbers[i] = numbers[i] - numbers[i + 1];
+                        operators.splice(i, 1);
+                        numbers.splice(i + 1, 1);
+                    }
+                    break;
+            }
+        }
+    }
+
+    input.innerHTML = numbers[0].toString().replace(".", ",");
+    equals = true;
+
+    // While result is longer than/equals 16 characters, shorten result
+    while (input.textContent.length >= 16) {
+        input.textContent = input.textContent.substring(0, input.textContent.length - 1);
+    }
 }
 
 // Remove button
 function remove() {
     var input = document.getElementById("input");
 
-    input.innerText = input.innerText.substring(0, input.innerText.length - 1);
+    input.textContent = input.textContent.substring(0, input.textContent.length - 1);
 
     // Check if comma exists
     if (input.innerHTML.indexOf(",") == -1)
     {
         comma = false;
+    }
+
+    // If equals
+    if (equals) {
+        document.getElementById("calculation").innerHTML = "";
+        numbers = [];
+        operators = [];
+        equals = false;
     }
 }
 
@@ -121,10 +234,21 @@ function remove() {
 function reset() {
     document.getElementById("input").innerHTML = "";
     comma = false;
+
+    // If equals
+    if (equals) {
+        document.getElementById("calculation").innerHTML = "";
+        numbers = [];
+        operators = [];
+        equals = false;
+    }
 }
 
 function resetFull() {
     document.getElementById("input").innerHTML = "";
     document.getElementById("calculation").innerHTML = "";
     comma = false;
+    equals = false;
+    numbers = [];
+    operators = [];
 }
